ec29bd4261e2b5e8d1d1878b202d90d18ec6e73b
Vlad Firoiu <vladfi2@gmail.com>
2016-03-11 14:00:35 -0500

    Periodically save and show validation loss.

--- Hand/simple_tf.py
+++ Hand/simple_tf.py
@@ -9,7 +9,7 @@ width = 256
 height = 256
 channels = 3
 param_size = 4
-batch_size = 5
+batch_size = 4
 
 """
 image_input = tf.placeholder(tf.float32, shape=[width, height, channels])
@@ -24,7 +24,7 @@ image_batch, target_batch = queue.dequeue_many(batch_size)
 image_batch = tf.placeholder(tf.float32, shape=[None, width, height, channels])
 target_batch = tf.placeholder(tf.float32, shape=[None, param_size])
 
-channels = [64, 64, 32, 16]
+channels = [64, 32, 16, 8]
 conv = image_batch
 for c in channels:
   conv = tfl.convLayer(conv, filter_depth=c)
@@ -37,7 +37,7 @@ print(flat_size)
 conv_flat = tf.reshape(conv, [-1, flat_size])
 #conv_flat = tf.reshape(conv, [batch_size, -1])
 
-fc1 = tfl.affineLayer(conv_flat, 512, nl=tf.nn.tanh)
+fc1 = tfl.affineLayer(conv_flat, 256, nl=tf.nn.tanh)
 fc2 = tfl.affineLayer(fc1, param_size)
 
 # quaternion distance
@@ -69,8 +69,8 @@ data_size = len(params)
 image_files = [directory + str(i) + '.png' for i in range(data_size)]
 #filename_queue = tf.train.string_input_producer(filenames)
 
-#validation = min(100, data_size // 10)
-validation = 0
+validation = min(100, data_size // 10)
+#validation = 0
 training = data_size - validation
 
 def makeBatch(queue, size):
@@ -83,20 +83,42 @@ def makeBatch(queue, size):
     image_batch : np.array(images),
     target_batch : np.array(targets)
   }
-"""
+
 def validate():
-  images = map(readPNG, image_files[-validation:])
+  validation_files = image_files[-validation:]
+  validation_targets = params[-validation:]
   
-  feed_dict = {
-    image_batch : tf.pack(images),
-"""
+  validation_loss = 0.0
+  i = 0
+  
+  while i < validation:
+    j = min(validation, i + batch_size)
+    images = map(readPNG, validation_files[i:j])
+    
+    feed_dict = {
+      image_batch : np.array(images),
+      target_batch : np.array(validation_targets[i:j])
+    }
+    
+    validation_loss += sess.run(loss, feed_dict)
+    i = j
+  
+  return 1.0 + validation_loss / validation
 
+train_queue = ModQueue(training)
 
-queue = ModQueue(validation)
-
+saver = tf.train.Saver(tf.all_variables())
 sess.run(tf.initialize_all_variables())
 
-
-for i in range(2):
-  t, l = sess.run([trainGD, loss], makeBatch(queue, 2))
-  print(l)
+counter = 0
+while True:
+  for i in range(100):
+    t, l = sess.run([trainGD, loss], makeBatch(train_queue, batch_size))
+    print("Training loss:", 1.0 + l / batch_size)
+  
+  print("Validation loss:", validate())
+  
+  saver.save(sess, 'Saves/simple', global_step = counter)
+  
+  counter += 1
+  
