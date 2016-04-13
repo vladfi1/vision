c8efa781443bf52fb5eea0d8c1bcfa180d349dbc
Vlad Firoiu <vladfi2@gmail.com>
2016-04-13 15:50:41 -0400

    Randomize fingers.

--- Hand/hand.py
+++ Hand/hand.py
@@ -34,7 +34,7 @@ def printBone(root):
 
 #printBone(handR)
 
-def randomizeCamera():
+def randomizeCamera(params=None):
   "Pick a random orientation to look at the hand"
   # default camera heading
   v = mathutils.Vector((0, 0, 1))
@@ -47,7 +47,10 @@ def randomizeCamera():
   camera.rotation_mode = 'QUATERNION'
   camera.rotation_quaternion = q
   
-  return q
+  if params is not None:
+    params['camera'] = list(q)
+  
+  return list(q)
 
 def resetBone(bone):
   if bone.rotation_mode == 'QUATERNION':
@@ -55,31 +58,97 @@ def resetBone(bone):
   else:
     bone.rotation_euler.zero()
 
+def sampleTheta():
+  return numpy.random.beta(1, 4)
+
 def randomizeBone(bone):
+  #bone.rotation_mode = 'QUATERNION'
+  #bone.rotation_mode = 'XYZ'
+  
+  """
   if bone.rotation_mode == 'QUATERNION':
-    bone.rotation_quaternion = randomQuaternion().slerp(qid, 0.6)
+    q = randomQuaternion().slerp(qid, 0.85)
+    bone.rotation_quaternion = q
+    return q
+  """
+  
+  if bone.name.startswith('finger'):
+    bone.rotation_mode = 'XYZ'
+    theta = sampleTheta()
+    
+    bone.rotation_euler.x = theta
+    bone.rotation_euler.y = numpy.random.normal(0, 0.03)
+    bone.rotation_euler.z = theta
+    return bone.rotation_euler
+  
+  for i in range(1, 3):
+    if bone.name == 'thumb.0%d.R' % i:
+      bone.rotation_mode = 'XYZ'
+      theta = sampleTheta()
+      bone.rotation_euler.x = theta
+      
+      if i == 1:
+        bone.rotation_euler.z = -theta
+    
+      return bone.rotation_euler
+  
+  if bone.name == 'thumb.01.R.001':
+    bone.rotation_mode = 'XYZ'
+    theta = sampleTheta()
+    bone.rotation_euler.x = -theta
+    
+    return bone.rotation_euler
+  
+  return None
 
-for bone in bonesR:
-  resetBone(bone)
+def resetScene():
+  for bone in bonesR:
+    resetBone(bone)
 
-#for bone in bonesR:
-#  randomizeBone(bone)
+def randomizeHand(params=None):
+  if params == None:
+    params = {}
 
-#randomizeCamera()
+  for bone in bonesR:
+    theta = randomizeBone(bone)
+    if theta is not None:
+      params[bone.name] = list(theta)
+  
+  return params
+
+def randomizeScene():
+  params = randomizeHand()
+  randomizeCamera(params)
+  return params
+
+bpy.context.scene.render.image_settings.file_format = 'JPEG'
 
 def render(name):
-  bpy.data.scenes['Scene'].render.filepath = '%s.png' % name
+  bpy.data.scenes['Scene'].render.filepath = '%s.jpeg' % name
   bpy.ops.render.render( write_still=True )
 
 def genSimple(number):
   params = []
 
   for index in range(number):
-    params.append(list(randomizeCamera()))
+    #params.append(list(randomizeCamera()))
+    randomizeHand()
     render(index)
 
   with open('params', 'wb') as params_file:
     pickle.dump(params, params_file)
 
-genSimple(1000)
+def genTiered(count, viewpoints):
+  tiers = []
+  for c in range(count):
+    hand = randomizeHand()
+    cameras = []
+    for v in range(viewpoints):
+      cameras.append(randomizeCamera())
+      render("%d-%d" % (c, v))
+    tiers.append((hand, cameras))
+  
+  with open('params', 'wb') as params_file:
+    pickle.dump(tiers, params_file)
 
+genTiered(10, 5)
