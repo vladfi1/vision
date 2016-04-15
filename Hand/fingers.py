c538a0e1f9c4a0c5162be6d0cb1383bd1c9b88cc
Vlad Firoiu <vladfi2@gmail.com>
2016-04-15 16:17:01 -0400

    Finger info.

---
+++ Hand/fingers.py
@@ -0,0 +1,22 @@
+fingers = [
+  'finger_index.01.R',
+  'finger_index.02.R',
+  'finger_index.03.R',
+
+  'thumb.01.R.001',
+  'thumb.01.R',
+  'thumb.02.R',
+  'thumb.03.R',
+
+  'finger_middle.01.R',
+  'finger_middle.02.R',
+  'finger_middle.03.R',
+
+  'finger_ring.01.R',
+  'finger_ring.02.R',
+  'finger_ring.03.R',
+
+  'finger_pinky.01.R',
+  'finger_pinky.02.R',
+  'finger_pinky.03.R'
+]
