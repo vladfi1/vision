98724bb60511a9029d99435f055ec31cbffaba31
Vlad Firoiu <vladfi2@gmail.com>
2016-05-02 13:25:03 -0400

    Limit data size.

--- Hand/simple_features.py
+++ Hand/simple_features.py
@@ -11,12 +11,14 @@ features_file = data_dir + 'features'
 with open(features_file, 'rb') as f:
   features = np.squeeze(np.load(f))
 
-data_size = len(params)
+#data_size = len(params)
+data_size = 200
 
-test_size = min(100, data_size // 10)
+#test_size = min(100, data_size // 10)
+test_size = 100
 test_params = params[:test_size]
 test_features = features[:test_size]
 
-train_params = params[test_size:]
-train_features = features[test_size:]
+train_params = params[test_size:data_size]
+train_features = features[test_size:data_size]
 
