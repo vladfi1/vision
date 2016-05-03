f60309b4be0017bd50e7a5d6127d9f9fe1af1c6c
Vlad Firoiu <vladfi2@gmail.com>
2016-05-02 21:05:34 -0400

    convnet on top of pool_1 to predict fingers and camera pose

--- Hand/fingers_inception.py
+++ Hand/fingers_inception.py
@@ -3,85 +3,126 @@ import tf_lib as tfl
 import six
 import pickle
 import numpy as np
-from fingers import *
-from inception import *
+import inception
+import scene
 
-finger_size = 3
-camera_size = 4
+target = scene.placeholder(scene.sceneParams2, [None], 'target')
 
-with tf.variable_scope('target'):
-  target_fingers = {finger : tf.placeholder(tf.float32, [finger_size], name=finger) for finger in fingers}
-  #target_fingers_concat = tf.concat(target_fingers)
-  target_camera = tf.placeholder(tf.float32, [camera_size], name='camera')
+op = 'pool_1'
+#features = tf.reshape(pool_3, [1, -1], name='features')
+input_features = tf.placeholder(tf.float32, [None] + inception.getShape(op), name='features')
+
+with tf.variable_scope('infer'):
+  x = input_features
+  for i, f in enumerate([128, 64, 32]):
+    with tf.variable_scope('conv%d' % i):
+      x = tfl.convLayer(x, filter_depth=f)
   
-features = tf.reshape(pool_3, [1, -1], name='features')
-
-with tf.variable_scope('predict'):
+  flat = tfl.flatten(x)
+  
   with tf.variable_scope('fc1'):
-    fc1 = tfl.affineLayer(features, 1024, tf.tanh)
+    fc1 = tfl.affineLayer(flat, 1024, tf.tanh)
   
-  predict_fingers = {finger : tf.squeeze(tfl.affineLayer(fc1, finger_size), name=finger) for finger in fingers}
+  predict = {}
+  for name, n in scene.fingerParams:
+    with tf.variable_scope(name):
+      predict[name] = tfl.affineLayer(fc1, n)
   
   with tf.variable_scope('camera'):
-    predict_camera = tf.squeeze(tfl.affineLayer(fc1, camera_size, tf.tanh))
+    predict['camera'] = tfl.affineLayer(fc1, 4, tf.tanh)
 
 with tf.name_scope('loss'):
-  loss_fingers = {finger : tf.squared_difference(predict_fingers[finger], target_fingers[finger], name=finger) for finger in fingers}
+  loss_fingers = {}
+  for name, _ in scene.fingerParams:
+    with tf.name_scope(name):
+      loss = tf.squared_difference(predict[name], target[name])
+      loss = tf.reduce_sum(loss, 1)
+      loss = tf.reduce_mean(loss)
+      loss_fingers[name] = loss
   
-  loss_fingers_total = tf.reduce_sum(tf.concat(0, [loss_fingers[finger] for finger in fingers]))
+  with tf.name_scope('fingers_total'):
+    xs = [tf.reshape(x, [1]) for x in loss_fingers.values()]
+    loss_fingers_total = tf.reduce_sum(tf.concat(0, xs))
   
   with tf.name_scope('camera'):
-    loss_camera = tfl.quaternionDistance(predict_camera, target_camera)
+    loss_camera = tf.reduce_mean(tfl.quaternionDistance(predict['camera'], target['camera']))
   
-  loss_total = loss_camera + loss_fingers_total
+  with tf.name_scope('total'):
+    loss_total = loss_camera + loss_fingers_total
 
-trainRMS = tf.train.RMSPropOptimizer(0.0001, momentum=0.1).minimize(loss_total)
+global_step = tf.Variable(0, name='global_step', trainable=False)
+adamOpt = tf.train.AdamOptimizer(10.0 ** -4)
+# train_q = opt.minimize(qLoss, global_step=global_step)
+# opt = tf.train.GradientDescentOptimizer(0.0)
+grads_and_vars = adamOpt.compute_gradients(loss_total)
+trainAdam = adamOpt.apply_gradients(grads_and_vars, global_step=global_step)
 
-directory = 'Data/Fingers/'
+#trainRMS = tf.train.RMSPropOptimizer(0.0001, momentum=0.1).minimize(loss_total)
 
-params = None
-with open(directory + 'params', 'rb') as f:
+data_dir = 'Data/Fingers/'
+
+params_file = data_dir + 'params'
+with open(params_file, 'rb') as f:
   params = pickle.load(f)
 
-data_size = len(params)
+N = len(params)
 
-image_files = [directory + str(i) + '.jpeg' for i in range(data_size)]
-data = zip(image_files, params)
+features = inception.loadFeatures(data_dir, N, op)
 
-validation = min(100, data_size // 10)
-validation_files = image_files[-validation:]
-validation_targets = params[-validation:]
+data_size = N
+#data_size = 200
 
-training = data_size - validation
-train_queue = six.moves.queue.ModQueue(training)
+#test_size = min(100, data_size // 10)
+test_size = 100
+train_size = data_size - test_size
 
-def feedDict(i):
-  p = params[i]
-  feed_dict = {target_fingers[finger] : np.array(p[finger]) for finger in fingers}
-  feed_dict[target_camera] = np.array(p['camera'])
-  feed_dict[image_str] = tfl.readImage(image_files[i])
+batch_size = 5
+
+train_queue = six.moves.queue.ModQueue(train_size // batch_size)
+
+def feedBatch(index, train=True):
+  index *= batch_size
+  if train:
+    index += test_size
+  feed_dict = {input_features : features[index:index+batch_size]}
+  scene.feed(scene.sceneParams2, 'target', params[index:index+batch_size], feed_dict)
+  
   return feed_dict
 
-def validate():
-  validation_loss = 0.0
+def test():
+  batches = test_size // batch_size
   
-  for i in range(data_size - validation, data_size):
-    validation_loss += sess.run(loss_total, feedDict(i))
+  predictions = []
+  test_loss = 0.0
+
+  for i in range(batches):
+    feed_dict = feedBatch(i, False)
+    predictions += scene.read(scene.sceneParams2, predict, sess, feed_dict)
+    test_loss += sess.run(loss_total, feed_dict)
   
-  return validation_loss / validation
+  return predictions, test_loss / batches
 
 saver = tf.train.Saver(tf.all_variables())
 
+sess = tf.Session()
+
 def train():
-  counter = 0
   while True:
     for i in range(500):
-      t, f, c = sess.run([trainRMS, loss_fingers_total, loss_camera], feedDict(train_queue.dequeue()))
+      t, f, c = sess.run([trainAdam, loss_fingers_total, loss_camera], feedBatch(train_queue.dequeue()))
       print(f, c)
   
-    print("Validation loss:", validate())
+    predictions, test_loss = test()
     
-    saver.save(sess, 'Saves/fingers_inception', global_step = counter)
+    print("Test loss:", test_loss)
     
-    counter += 1
+    with open(data_dir + 'predict', 'wb') as f:
+      pickle.dump(predictions, f)
+    
+    saver.save(sess, 'Saves/fingers_inception')
 
+def init():
+  sess.run(tf.initialize_all_variables())
+
+def restore():
+  saver.restore(sess, 'Saves/fingers_inception')
